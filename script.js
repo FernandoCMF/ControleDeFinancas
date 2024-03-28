eabdabebdfb9bb5c3349ff33b9577f37950dbcac
Nando <fmfernando61@gmail.com>
2024-03-27 21:38:15 -0300

    adicionando metodo PUT na funcao savaTransaction

--- script.js
+++ script.js
@@ -72,20 +72,40 @@ document.addEventListener('DOMContentLoaded', setup)
 async function savaTransaction(ev) {
   ev.preventDefault()
 
+  const id = document.querySelector('#id').value
   const name = document.querySelector('#name').value
   const amount = parseFloat(document.querySelector('#amount').value)
 
-  const response = await fetch('http://localhost:3000/transactions', {
-    method: 'POST',
-    body: JSON.stringify({ name, amount }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+  if (id) {
+    const response = await fetch('http://localhost:3000/transactions', {
+      method: 'PUT',
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
 
-  const transaction = await response.json()
-  transactions.push(transaction)
-  renderTransaction(transaction)
+    const transaction = await response.json()
+    const indexToRemove = transactions.findIndex((t) => t.id === id)
+    transactions.splice(indexToRemove, 1, transaction)
+    document.querySelector(`#transaction-${id}`).remove()
+    renderTransaction(transaction)
+  } else {
+
+    const response = await fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const transaction = await response.json()
+    transactions.push(transaction)
+    renderTransaction(transaction)
+  }
+
+
 
   ev.target.reset()
   updateBalance()
